463650b4a44467b438aa36da524f06189d1dd9a4
Sam Kington <github@illuminated.co.uk>
2011-12-18 17:21:34 +0000

    No hard-coded paths.

--- starman.sh
+++ starman.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+# Find out where we're unpacked (thanks to
+# http://stackoverflow.com/questions/59895 for this)
 
+DANCERDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 killall plackup;
-/usr/local/bin/plackup \
-    --server Starman --app /home/sam/dancer/bin/app.pl \
+plackup \
+    --server Starman --app $DANCERDIR/bin/app.pl \
     --port 80 --enviroment development \
-    --reload --Reload /home/sam/dancer,/home/sam/dancer/lib \
-    --access_log /home/sam/dancer/logs/access_log
+    --reload --Reload $DANCERDIR,$DANCERDIR/lib \
+    --access_log $DANCERDIR/logs/access_log
 
